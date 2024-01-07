1edbe5c3e2747f0ff608b81812e40044aed728f3
Nick Mavrakis <mavrakis@hyperlocalhq.com>
2024-01-07 10:52:01 +0200

    Revert "🔨 Remove providing_args argument from Signal() instantiation"
    This reverts commit e8a160bc910ffc787010a2bcafbf47cf2980655f.

--- river/signals.py
+++ river/signals.py
@@ -10,14 +10,14 @@ from river.models.on_approved_hook import OnApprovedHook
 from river.models.on_complete_hook import OnCompleteHook
 from river.models.on_transit_hook import OnTransitHook
 
-pre_on_complete = Signal()
-post_on_complete = Signal()
+pre_on_complete = Signal(providing_args=["workflow_object", "field_name", ])
+post_on_complete = Signal(providing_args=["workflow_object", "field_name", ])
 
-pre_transition = Signal()
-post_transition = Signal()
+pre_transition = Signal(providing_args=["workflow_object", "field_name", "source_state", "destination_state"])
+post_transition = Signal(providing_args=["workflow_object", "field_name", "source_state", "destination_state"])
 
-pre_approve = Signal()
-post_approve = Signal()
+pre_approve = Signal(providing_args=["workflow_object", "field_name", "transition_approval"])
+post_approve = Signal(providing_args=["workflow_object", "field_name", "transition_approval", "transition_approval_meta"])
 
 LOGGER = logging.getLogger(__name__)
 
